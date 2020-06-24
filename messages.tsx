76237e33d04f3a9dde3325023b85c7faab4b46e5
Enmanuel Cueva <@>
2020-06-24 12:22:43 +0200

    updated messages

--- messages.tsx
+++ messages.tsx
@@ -1,2 +1,12 @@
-export const SERVER_FETCH_ITEMS = 'SERVER_FETCH_ITEMS';
+/**
+ * List of available communication messages from SERVER TO CLIENT
+ */
+
 export const CLIENT_RELOAD_ITEMS = 'CLIENT_RELOAD_ITEMS';
+
+/**
+ * List of available communication messages from CLIENT TO SERVER
+ */
+
+export const SERVER_AVAILABLE_SHOPPING_LISTS = 'SERVER_AVAILABLE_SHOPPING_LISTS';
+export const SERVER_FETCH_ITEMS = 'SERVER_FETCH_ITEMS';
