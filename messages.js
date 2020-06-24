8ad0a94b4dd9580fb4c859ccec9b04c8ec75abd9
Enmanuel Cueva <@>
2020-06-24 13:30:15 +0200

    updated messages

---
+++ messages.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * List of available communication messages from SERVER TO CLIENT
+ */
+exports.__esModule = true;
+exports.CLIENT_RELOAD_ITEMS = 'CLIENT_RELOAD_ITEMS';
+/**
+ * List of available communication messages from CLIENT TO SERVER
+ */
+exports.SERVER_AVAILABLE_SHOPPING_LISTS = 'SERVER_AVAILABLE_SHOPPING_LISTS';
+exports.SERVER_FETCH_ITEMS = 'SERVER_FETCH_ITEMS';
